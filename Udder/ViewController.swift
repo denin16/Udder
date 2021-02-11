80be6505132fb6afe11824f2b7c118740831d8dd
denin <navdeep.beniwal1997@gmail.com>
2021-02-11 19:04:35 +0530

    Add ViewWillAppear and ViewWillDisappear

--- Udder/ViewController.swift
+++ Udder/ViewController.swift
@@ -14,6 +14,12 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        // View will appear
+    }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        // View will disappear
+    }
 }
 
